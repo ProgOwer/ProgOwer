ac9948d64e9325ac0e3e92a298949a70f4b6411c
ProgOwer <ProgOwer0770@gmail.com>
2021-05-19 20:49:53 +0200

    Add Git Script

---
+++ git.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Git Disable SSL Verify
+git config --global http.sslverify false
+
+
+# Git Setup Multiple Repository
+git remote add gitlab git@gitlab.com:ProgOwer/progower.git
+git remote set-url --add --push origin git@gitlab.com:ProgOwer/progower.git
+
+git remote add github git@github.com:ProgOwer/ProgOwer.git
+git remote set-url --add --push origin git@github.com:ProgOwer/ProgOwer.git
+
+git remote add neonlit ssh://git@git.neonlit.fr:2222/progower/progower.git
+git remote set-url --add --push origin ssh://git@git.neonlit.fr:2222/progower/progower.git
+
+
+# Display Config
+git remote show origin
+
+git config --list
